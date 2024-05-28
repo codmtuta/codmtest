aafb7b975463b8e484588476f875db5d80080f48
Your Name <you@example.com>
2024-05-28 14:19:50 +0300

    v012

---
+++ js/wallet.js
@@ -0,0 +1,34 @@
+const transactions = document.querySelectorAll('.wallet__transaction');
+
+transactions.forEach(el => {
+    el.addEventListener('click', () => {
+        transactions.forEach(event => {
+            if (event != el) {
+                event.classList.remove('activeDetails');
+            }
+        });
+        el.classList.toggle('activeDetails');
+    });
+});
+
+const openConclusion = document.querySelector('.wallet__conclusion'),
+    openRefill = document.querySelector('.wallet__refill'),
+    btnConclusion = document.querySelector('.wallet__btn-conclusion'),
+    btnRefill = document.querySelector('.wallet__btn-refill'),
+    closeWindow = document.querySelectorAll('.wallet__new-cancel');
+
+function openWindowWallet(el, btn, closeBtn) {
+    el.style.display = 'none';
+    btn.addEventListener('click', () => {
+        el.style.display = 'block';
+    });
+
+    closeBtn.forEach(close => {
+        close.addEventListener('click', () => {
+            el.style.display = 'none';
+        });
+    });
+};
+
+openWindowWallet(openConclusion, btnConclusion, closeWindow);
+openWindowWallet(openRefill, btnRefill, closeWindow);
